722dc5d38a5ab24f59526b56e200ffcaea4edb83
Leonardo <demetrio.vd@hotmail.com>
2020-06-04 08:44:54 -0300

    Edicao do README

--- Requisicao CEP com tela/README.md
+++ Requisicao CEP com tela/README.md
@@ -9,3 +9,20 @@ O codigo faz uma requisição ao site do VIACEP através da sua API, com isso el
 				print('Erro 400')
 
 			endereco = url.json()
+			
+			
+			
+![TELA DE CEP](https://i.imgur.com/BW7YGrb.png "TELA DE CEP")
+
+
+	def __init__(self):
+
+        layout = [
+                [sg.Text('cep'),sg.Input(size = (25,0), key='CEP')],
+                [sg.Button('Buscar')],
+                [sg.Output(size=(40,10))]
+        ]
+
+
+        self.tela = sg.Window('Busca de CEP',layout)
+		self.button, self.values = self.tela.Read()
--- requisicaoCEP.py
+++ requisicaoCEP.py
@@ -1,19 +1,49 @@
 import requests
 import json
+import PySimpleGUI as sg
 
-def consulta_cep(cep):
-    url = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
-    if url.status_code == 200:
-    	print('Sucesso')
-    elif url.status_code == 400:
-    	print('Erro 400')
 
-    endereco = url.json()
+class TelaPython:
+    def __init__(self):
 
-    for k, v in endereco.items():
-    	print(k , ' : ' ,v )
-    	
+        layout = [
+                [sg.Text('cep'),sg.Input(size = (25,0), key='CEP')],
+                [sg.Button('Buscar')],
+                [sg.Output(size=(40,10))]
+        ]
 
-if __name__ == '__main__':
-	cep = input('Digite um cep')
-    consulta_cep('cep')
+
+        self.tela = sg.Window('Busca de CEP',layout)
+        
+
+
+
+    def consultacep(self,cep):
+
+        url = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+        if url.status_code == 200:
+        	print('Sucesso')
+        elif url.status_code == 400:
+        	print('Erro 400')
+
+        endereco = url.json()
+
+        return endereco
+
+
+    def start_window(self):
+        while True:
+                
+            self.button , self.values = self.tela.Read()
+          
+            try:    
+                valores = self.consultacep(self.values['CEP'])
+                for k, v in valores.items():
+                    print(k.upper() , ':' ,v)
+               
+            except:
+                print('Name Error, funcao não definida')
+                
+
+jn = TelaPython()
+jn.start_window()
